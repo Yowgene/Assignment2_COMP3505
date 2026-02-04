a74eb27e6bad2f28588ad0f73fc8d46fbe011be1
ahad-c <90429221+ahad-c@users.noreply.github.com>
2026-02-04 13:34:09 -0700

    getCentralValue(), getlength(), contains(double)

--- JFreeChart/test/org/jfree/data/RangeTest.java
+++ JFreeChart/test/org/jfree/data/RangeTest.java
@@ -10,27 +10,89 @@ import org.junit.jupiter.api.Test;
 class RangeTest {
 	
 	private Range exampleRange;
-	@BeforeAll
-	static void setUpBeforeClass()throws Exception{
-	
-	}
-	@BeforeEach
-	void setUp() throws Exception {
-	}
 
-	@Test
-	void test() {
-		exampleRange = new Range(-1, 1);
-		assertEquals(0, exampleRange.getCentralValue(), 0.1d, "The central value of (-1,1) is 0");
-	}
-	
-	@AfterEach
-	void tearDown() throws Exception {
-		
-	}
-	
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-		
-	}
+    @BeforeEach
+    void setUp() {
+        exampleRange = new Range(-1, 1);
+    }
+    
+    /*
+     * getCentralValue()
+     */
+    @Test
+    void testGetCentralValueSymmetricRange() {
+        assertEquals(0, exampleRange.getCentralValue(), 0.0001,
+                "The central value of (-1, 1) should be 0");
+    }
+
+    @Test
+    void testGetCentralValuePositiveRange() {
+        exampleRange = new Range(2, 6);
+        assertEquals(4, exampleRange.getCentralValue(), 0.0001,
+                "The central value of (2, 6) should be 4");
+    }
+
+    @Test
+    void testGetCentralValueZeroLengthRange() {
+        exampleRange = new Range(3, 3);
+        assertEquals(3, exampleRange.getCentralValue(), 0.0001,
+                "The central value of (3, 3) should be 3");
+    }
+    
+    /*
+     * getLength()
+     */
+    @Test
+    void testGetLengthNormalRange() {
+        exampleRange = new Range(1, 5);
+        assertEquals(4, exampleRange.getLength(), 0.0001,
+                "The length of (1, 5) should be 4");
+    }
+
+    @Test
+    void testGetLengthZeroRange() {
+        exampleRange = new Range(3, 3);
+        assertEquals(0, exampleRange.getLength(), 0.0001,
+                "The length of (3, 3) should be 0");
+    }
+    
+    /*
+     * contains(double)
+     */
+
+    @Test
+    void testContainsValueInsideRange() {
+        exampleRange = new Range(1, 5);
+        assertTrue(exampleRange.contains(3),
+                "Range (1,5) should contain 3");
+    }
+
+    @Test
+    void testContainsValueAtLowerBound() {
+        exampleRange = new Range(1, 5);
+        assertTrue(exampleRange.contains(1),
+                "Range (1,5) should contain its lower bound");
+    }
+
+    @Test
+    void testContainsValueAtUpperBound() {
+        exampleRange = new Range(1, 5);
+        assertTrue(exampleRange.contains(5),
+                "Range (1,5) should contain its upper bound");
+    }
+
+    @Test
+    void testContainsValueBelowRange() {
+        exampleRange = new Range(1, 5);
+        assertFalse(exampleRange.contains(0),
+                "Range (1,5) should not contain values below the lower bound");
+    }
+
+    @Test
+    void testContainsValueAboveRange() {
+        exampleRange = new Range(1, 5);
+        assertFalse(exampleRange.contains(6),
+                "Range (1,5) should not contain values above the upper bound");
+    }
+    
 }
