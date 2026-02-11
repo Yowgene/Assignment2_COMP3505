dcaa75a562eb42e4dd7f7bf6122e468936ad3954
seph <seph@localhost>
2026-02-11 11:52:53 -0700

    Added more tests

--- JFreeChart/test/org/jfree/data/RangeTest.java
+++ JFreeChart/test/org/jfree/data/RangeTest.java
@@ -95,4 +95,45 @@ class RangeTest {
                 "Range (1,5) should not contain values above the upper bound");
     }
     
+    @Test
+    void testContainsNull() {
+    	exampleRange = new Range(null,null);
+    	assertTrue(exampleRange.contains(null), 
+    			"Range should not contain null value.");
+    }
+    
+    @Test
+    //This test is just an example, 2 is not constant
+    void testShiftUpperBound() {
+    	Range testRange = Range.shift(exampleRange, 2.0, true);
+    	assertTrue(3,testRange.getUpperBound(),0.001,
+    			"Range shift should not be more than 2!");
+    }
+    
+    
+    @Test
+    //This test is just an example, 2 is not constant
+    void testShiftLowerBound() {
+    	Range testRange = Range.shift(exampleRange,2.0,true);
+    	assertTrue(1,testRange.getLowerBound(),0.001,
+    			"Range shift should not be more than 2!");
+    }
+    
+    
+    @Test
+    //This test is just an example, 2 is not constant
+    void testConstrainUpper() {
+    	double test = exampleRange.constrain(5.0);
+    	assertTrue(test <= 1,
+    			"Range should not be over than the actual upper Range");
+    }
+    
+    @Test
+    //This test is just an example, 2 is not constant
+    void testConstrainLower() {
+    	double test = exampleRange.constrain(-5);
+    	assertTrue(test >= -1,
+    			"Range should not be over than the actual lower Range");
+    }
+    
 }
